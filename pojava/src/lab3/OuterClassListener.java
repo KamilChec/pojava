99e474f4e72e8a9583faca515b730319f1e4e184
Kamil Chęć <k.chec.prv@gmail.com>
2018-03-10 00:19:25 +0100

    dodanie przykładu 3

---
+++ pojava/src/lab3/OuterClassListener.java
@@ -0,0 +1,30 @@
+package lab3;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.HeadlessException;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+public class OuterClassListener extends JFrame {
+	
+	public OuterClassListener() throws HeadlessException {
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setSize(300,100);
+		
+		String[] colors = {"red", "green", "blue"};
+		JComboBox<String> colorList = new JComboBox<String>(colors);
+		this.add(colorList, BorderLayout.PAGE_START);
+        this.getContentPane().setBackground(Color.red);
+        
+        colorList.addItemListener(new ComboBoxItemListener((JPanel)this.getContentPane()));
+	}
+	
+	public static void main(String[] args) {
+		OuterClassListener frame = new OuterClassListener();
+		frame.setVisible(true);
+
+	}
+
+}
